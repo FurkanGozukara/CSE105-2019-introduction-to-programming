bf9d05594db22ac117bd2afec5874465d58812be
Furkan Gözükara <furkangozukara@gmail.com>
2019-09-16 10:18:43 +0300

    week 2 - v.1

---
+++ source codes/lecture 2 hello world/lecture 2 hello world/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace lecture_2_hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mySchoolName = "Toros University";
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(mySchoolName);
+            string myStudentId = "357";
+            Console.WriteLine("My student id is " + myStudentId);
+            Console.Write("First day of the school");
+            Console.Write("This is the first lesson");
+            Console.WriteLine("1");
+            Console.ReadKey();   
+        }   
+    }
+}
