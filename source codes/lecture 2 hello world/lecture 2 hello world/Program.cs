1054c8161e79826175fab815b794abbe50fb4cc5
Furkan Gözükara <furkangozukara@gmail.com>
2019-09-16 10:40:30 +0300

    week 2 - v.2

--- source codes/lecture 2 hello world/lecture 2 hello world/Program.cs
+++ source codes/lecture 2 hello world/lecture 2 hello world/Program.cs
@@ -7,14 +7,31 @@ namespace lecture_2_hello_world
         static void Main(string[] args)
         {
             string mySchoolName = "Toros University";
-
-            Console.WriteLine("Hello World!");
+            char charA = 'A';
+            string letterA = "A";
+            Console.WriteLine("Hello World! \t" + charA); //\t is a special character that puts tab
+            //this below line is same as above line just explicit conversation
+            //in c# only same variable types can be processed
+            Console.WriteLine("Hello World! \t" + charA.ToString());
             Console.WriteLine(mySchoolName);
             string myStudentId = "357";
             Console.WriteLine("My student id is " + myStudentId);
-            Console.Write("First day of the school");
+            Console.Write("\nFirst day of the school");
             Console.Write("This is the first lesson");
+            Console.Write("Manager 'said' \"follow the rules\"");
             Console.WriteLine("1");
+
+            Console.Clear();//this clears the console screen
+
+            int myStudentNumber = 325;
+            int myNationalId = 10001;
+
+            Console.WriteLine("my student number is: " + myStudentNumber);
+
+            Console.WriteLine("my national id is: " + myNationalId);
+
+            Console.WriteLine("sum of my student number and national id is: " + (myStudentNumber + myNationalId));
+
             Console.ReadKey();   
         }   
     }
