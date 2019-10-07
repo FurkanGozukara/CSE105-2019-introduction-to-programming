f6edd24e2a47e93169bf13e18de75c9d603fd1fc
Furkan Gözükara <furkangozukara@gmail.com>
2019-10-07 09:58:29 +0300

    lecture 5 - v 1

---
+++ source codes/lecture 5 for foreach while/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace lecture_5_for_foreach_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("for loop: " + i);
+                //Thread.Sleep(500);
+            }
+            Console.Clear();
+
+            List<int> lstNumbers = new List<int>();
+            lstNumbers.AddRange(Enumerable.Range(1, 100));
+
+            //this below equals to above code
+            //for (int i =1; i < 101; i++)
+            //{
+            //    lstNumbers.Add(i);
+            //}
+
+            foreach (var vrNumber in lstNumbers)
+            {
+                Console.WriteLine("foreach loop: " + vrNumber);
+                //Thread.Sleep(500);
+            }
+
+            do
+            {
+                int irIndex = 0;
+                Console.WriteLine("do while loop: " + lstNumbers[irIndex]);
+                Thread.Sleep(500);
+                irIndex++;
+                if (irIndex >= lstNumbers.Count)
+                    break;
+            } while (true);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ source codes/lecture 5 wpf and while loop/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace lecture_5_wpf_and_while_loop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void print_to_screen_with_for(object sender, RoutedEventArgs e)
+        {
+            var childTask = new Task(() =>
+            {
+                for (int i = 0; i < 10000000; i++)
+                {
+                    Thread.Sleep(500);
+
+                    Dispatcher.BeginInvoke(DispatcherPriority.Normal, new Action(() =>
+                    {
+                        lblFor.Content = "for loop: " + i;
+                    }));
+                }
+            }, TaskCreationOptions.AttachedToParent);
+            childTask.Start();
+        }
+    }
+}
