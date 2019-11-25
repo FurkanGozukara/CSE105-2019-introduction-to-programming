a72bcc39f501bf2c5c38e3b110d9af884ffd856c
Furkan Gözükara <furkangozukara@gmail.com>
2019-11-25 09:30:18 +0300

    lecture 12 v 2

---
+++ source codes/lecture 12/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using NCalc;
+
+namespace lecture_12
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            cbmBoxUsers.Items.Add("ahmet");
+            cbmBoxUsers.Items.Add("mehmet");
+            cbmBoxUsers.Items.Add("ali");
+            cbmBoxUsers.SelectedIndex = 0;
+        }
+
+        //write calculated result to the selected users file
+        //show calculated results in the listbox with reverse order
+
+        private void BtnCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            NCalc.Expression myExpression = new NCalc.Expression(txtMath.Text);
+            try
+            {
+                double dblResult = (double)myExpression.Evaluate();//casting object type into double type
+            }
+            catch (Exception E)
+            {
+
+                MessageBox.Show(E.Message.ToString());
+            }   
+        }
+    }
+}
